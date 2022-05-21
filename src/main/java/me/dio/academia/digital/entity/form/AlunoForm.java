b1b1596434d54c5467a906e3cef37d1a196ad2fa
Valsnei <valsnei@gmail.com>
2022-05-20 21:56:06 -0300

    commit inicial

---
+++ src/main/java/me/dio/academia/digital/entity/form/AlunoForm.java
@@ -0,0 +1,33 @@
+package me.dio.academia.digital.entity.form;
+
+import lombok.AllArgsConstructor;
+import lombok.Data;
+import lombok.NoArgsConstructor;
+import org.hibernate.validator.constraints.br.CPF;
+
+import javax.validation.constraints.NotEmpty;
+import javax.validation.constraints.Past;
+import javax.validation.constraints.Size;
+import java.time.LocalDate;
+
+@Data
+@NoArgsConstructor
+@AllArgsConstructor
+public class AlunoForm {
+
+  @NotEmpty(message = "Preencha o campo corretamente.")
+  @Size(min = 3, max=50, message = "'${validatedValue}' precisa estar entre {min} e {max} caracteres.")
+  private String nome;
+
+  @NotEmpty(message = "Preencha o campo corretamente.")
+  @CPF(message = "'${validatedValue}' inválido.")
+  private String cpf;
+
+  @NotEmpty(message = "Preencha o campo corretamente.")
+  @Size(min = 3, max=50, message = "'${validatedValue}' precisa estar entre {min} e {max} caracteres.")
+  private String bairro;
+
+  @NotEmpty(message = "Preencha o campo corretamente.")
+  @Past(message = "Data '${validatedValue}1 é inválida")
+  private LocalDate dataDeNascimento;
+}
